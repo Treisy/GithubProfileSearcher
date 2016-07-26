d0780f6b036f2013ae390cd65b24fb5857ff589c
Treisy J <treisyjimenez@gmail.com>
2016-07-26 09:12:22 -0600

    Github Service & Get User

---
+++ app/services/github.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+  private  username; //Github Username
+
+  constructor(private _http:Http){
+    this.username = 'treisy';
+  }
+
+  getUser(){
+    return this._http.get('https://api.github.com/users/'+this.username).map(res => res.json());
+  }
+}
